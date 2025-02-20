6cd32c5bd05b9f82d3e4adbd95e3f15edad17a96
CJ Caballero <caballeroceejay5@gmail.com>
2025-02-20 16:55:19 +0800

    add status

--- src/AttendanceUseCase.js
+++ src/AttendanceUseCase.js
@@ -18,21 +18,21 @@ const findObject = new FindObjectUseCase();
 const upsertObject = new UpsertUseCase();
 const now = new Date();
 
-function convertTo24Hour(time) {
-  const [timePart, meridian] = time.split(" "); // Split the time and AM/PM
-  let [hour, minute] = timePart.split(":").map(Number);
+// function convertTo24Hour(time) {
+//   const [timePart, meridian] = time.split(" "); // Split the time and AM/PM
+//   let [hour, minute] = timePart.split(":").map(Number);
 
-  if (meridian === "PM" && hour !== 12) {
-    hour += 12; // Add 12 to convert PM times except 12 PM
-  } else if (meridian === "AM" && hour === 12) {
-    hour = 0; // Convert 12 AM to 0 (midnight)
-  }
+//   if (meridian === "PM" && hour !== 12) {
+//     hour += 12; // Add 12 to convert PM times except 12 PM
+//   } else if (meridian === "AM" && hour === 12) {
+//     hour = 0; // Convert 12 AM to 0 (midnight)
+//   }
 
-  // Return in 24-hour format with zero-padding for hours and minutes
-  return `${hour.toString().padStart(2, "0")}:${minute
-    .toString()
-    .padStart(2, "0")}`;
-}
+//   // Return in 24-hour format with zero-padding for hours and minutes
+//   return `${hour.toString().padStart(2, "0")}:${minute
+//     .toString()
+//     .padStart(2, "0")}`;
+// }
 
 function convertTo24HourTime(time) {
   const [timePart, modifier] = time.split(" ");
@@ -101,27 +101,6 @@ class AttendanceUseCase {
     return new Date().toLocaleTimeString("en-US", options);
   }
 
-  computeCreditDeduction(undertimeMinutes) {
-    let conversionValue = 0.0;
-
-    for (let i = 1; i <= undertimeMinutes; i++) {
-      // Add 0.003 at 7th, 19th, 31st, 43rd, 55th minute of every hour
-      if (
-        i % 60 === 7 ||
-        i % 60 === 19 ||
-        i % 60 === 31 ||
-        i % 60 === 43 ||
-        i % 60 === 55
-      ) {
-        conversionValue += 0.003;
-      } else {
-        conversionValue += 0.002;
-      }
-    }
-
-    return conversionValue.toFixed(3).toString();
-  }
-
   getCurrentTime() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, "0");
@@ -145,26 +124,9 @@ class AttendanceUseCase {
       ] = line;
 
       // console.log("TIMESTAMP: ", timestamp);
-      console.log("userID: ", userID);
+      // console.log("userID: ", userID);
       // console.log("timeInOutStats: ", timeInOutStats);
       const deviceTime = timestamp.split(" ");
-      //   if (userID === "undefined") {
-      //     return Promise.resolve("OK");
-      //   }
-
-      // const users = findObject.execute("users", {
-      //         companyID: Number(userID),
-      //       })
-
-      //       if(users.length > 0){
-      //         users.map(item => {
-      //           const dailyRec = {
-      //             date: deviceTime[0],
-      //             timeIn: this.getCurrentTime(),
-
-      //           }
-      //         })
-      //       }
 
       if (userID === "undefined") {
         return Promise.resolve("OK");
@@ -191,57 +153,24 @@ class AttendanceUseCase {
           this.getCurrentTime()
         );
 
-        // if (users.length > 0) {
-        //   this.checkSchedule(
-        //     dailyTimeRec,
-        //     timeRecStats,
-        //     attendance,
-        //     query,
-        //     this.getCurrentTime()
-        //   );
-        // }
+        if (users.length > 0) {
+          this.checkSchedule(
+            dailyTimeRec,
+            timeRecStats,
+            attendance,
+            query,
+            this.getCurrentTime()
+          );
+        }
 
         try {
-          // console.log("employee: ", employee);
-          // const dayToDay = selectedDay[now.getDay()];
-          // const schedule = employee[0].schedule.find(
-          //   (sched) => sched.day === dayToDay
-          // );
-          // // console.log("attendance get: ", attendance);
-          // // console.log("emppp", Array.isArray(employee[0].schedule));
-          // // console.log("scheddd", schedule);
-          // if (Array.isArray(employee[0].schedule) && schedule) {
-          // if (
-          //   attendance.timeOut !== "--:--"
-          // ) {
-          //   const scheduleTimeOut24H = `${convertTo24Hour(
-          //     schedule.timeout
-          //   )}:00`;
-          //   // await handleLeaveWithPay(
-          //   //   findObject,
-          //   //   upsertObject,
-          //   //   convertTo24HourTime,
-          //   //   attendance,
-          //   //   schedule,
-          //   //   dailyTimeRec,
-          //   //   employee,
-          //   //   daily_time_record,
-          //   //   getOvertimeDuration,
-          //   //   scheduleTimeOut24H
-          //   // );
-          //   // return;
-          //   return Promise.resolve("OK");
-          // }
+          console.log("ATT: ", attendance);
           await upsertObject.execute("daily_time_record", attendance);
           return Promise.resolve("OK");
-          // } else {
-          //   console.log("Assign Schedule First");
-          // }
         } catch (error) {
           return Promise.reject("Attendace: ", error);
         }
       }
-      return `OK: ${total}`;
     }
   }
 
@@ -276,26 +205,6 @@ class AttendanceUseCase {
     }
   }
 
-  handleHolidays(listofHolidays, dailyTimeRec, timeRecStats) {
-    listofHolidays.forEach((holiday) => {
-      if (holiday.type) {
-        timeRecStats.push(holiday.type);
-      }
-    });
-  }
-
-  handleLeaves(listofLeaves, dailyTimeRec, timeRecStats) {
-    listofLeaves.forEach((leave) => {
-      if (leave.selectedType) {
-        timeRecStats.push(leave.selectedType);
-      }
-    });
-  }
-
-  shouldCheckSchedule(users) {
-    return;
-  }
-
   async checkSchedule(
     dailyTimeRec,
     timeRecStats,
@@ -303,41 +212,26 @@ class AttendanceUseCase {
     query,
     deviceTime
   ) {
-    const dayToDay = selectedDay[now.getDay()];
     const schedule = { timein: "9:00 AM", timeout: "6:00 PM" };
-    // employee[0].schedule.find(
-    //   (sched) => sched.day === dayToDay
-    // );
-
     if (schedule) {
-      const newTimeNow = convertTo24HourTime(deviceTime);
+      let [hours, minutes, _] = deviceTime.split(":").map(Number);
+      // const newTimeNow = convertTo24HourTime(deviceTime);
       const newSchedTimeIN = convertTo24HourTime(schedule.timein);
       const newSchedTimeOUT = convertTo24HourTime(schedule.timeout);
       const empName = attendance.user;
       const data = {
-        device: query.SN,
+        device: query.SN.toString(),
         logMessage: `Successfully Time In ${empName}`,
         result: "Time In",
       };
 
-      console.log("ATT: ", attendance);
-
       if (dailyTimeRec.length === 0) {
         if (
-          newTimeNow.hours > newSchedTimeIN.hours ||
-          (newTimeNow.hours === newSchedTimeIN.hours &&
-            newTimeNow.minutes > newSchedTimeIN.minutes)
+          hours > newSchedTimeIN.hours ||
+          (hours === newSchedTimeIN.hours && minutes > newSchedTimeIN.minutes)
         ) {
           timeRecStats.push("LATE");
 
-          const lateMinutes =
-            (newTimeNow.hours - newSchedTimeIN.hours) * 60 +
-            (newTimeNow.minutes - newSchedTimeIN.minutes);
-          console.log("LATE: ", lateMinutes);
-
-          const conversionValue = this.computeCreditDeduction(lateMinutes);
-          console.log("late Deduction: ", conversionValue);
-          attendance.lateMinutes = conversionValue;
           try {
             await upsertObject.execute("biometric_logs", data);
           } catch (e) {
@@ -346,78 +240,24 @@ class AttendanceUseCase {
         }
         // log time in
       } else if (dailyTimeRec.length > 0) {
-        const timeIn = convertTo24HourTime(dailyTimeRec[0].timeIn);
-        const timeOut = convertTo24HourTime(dailyTimeRec[0].timeOut);
-        // if (listofSuspension.length > 0) {
-        //   timeRecStats.push("SUSPENDED");
-        // }
+        // const timeIn = convertTo24HourTime(dailyTimeRec[0].timeIn);
+        let [timeInHour, timeInMinute, _] = dailyTimeRec[0]?.timeIn
+          .split(":")
+          .map(Number);
         if (
-          timeIn.hours > newSchedTimeIN.hours ||
-          (timeIn.hours === newSchedTimeIN.hours &&
-            timeIn.minutes > newSchedTimeIN.minutes)
+          timeInHour > newSchedTimeIN.hours ||
+          (timeInHour === newSchedTimeIN.hours &&
+            timeInMinute > newSchedTimeIN.minutes)
         ) {
           timeRecStats.push("LATE");
         }
 
-        let lateMinutes =
-          (newSchedTimeOUT.hours - newTimeNow.hours) * 60 +
-          (newSchedTimeOUT.minutes - newTimeNow.minutes);
-
-        if (lateMinutes < 0) {
-          lateMinutes = 0;
-        }
-
-        let stillLate =
-          (newSchedTimeOUT.hours - timeOut.hours) * 60 +
-            (newSchedTimeOUT.minutes - timeOut.minutes) || 0;
-        if (stillLate < 0) {
-          stillLate = 0;
-        }
-
         if (
-          newTimeNow.hours < newSchedTimeOUT.hours ||
-          (newTimeNow.hours === newSchedTimeOUT.hours &&
-            newTimeNow.minutes < newSchedTimeOUT.minutes)
+          hours < newSchedTimeOUT.hours ||
+          (hours === newSchedTimeOUT.hours && minutes < newSchedTimeOUT.minutes)
         ) {
-          lateMinutes =
-            (newSchedTimeOUT.hours - newTimeNow.hours) * 60 +
-            (newSchedTimeOUT.minutes - newTimeNow.minutes);
-          stillLate =
-            (newTimeNow.hours - timeOut.hours) * 60 +
-            (newTimeNow.minutes - timeOut.minutes);
-
-          // if (dailyTimeRec[0].isTimeOut === true) {
-          //   underTime = parseFloat(dailyTimeRec[0].lateMinutes || 0);
-          //   attendance.newlyHiredDeduct = dailyTimeRec[0].newlyHiredDeduct;
-          // } else {
-          //   underTime =
-          //     parseFloat(dailyTimeRec[0].lateMinutes || 0) +
-          //     parseFloat(conversionValue);
-          // }
           timeRecStats.push("UNDERTIME");
         }
-        const conversionValue = this.computeCreditDeduction(lateMinutes);
-        let underTime = 0;
-
-        if (dailyTimeRec[0].isTimeOut === true) {
-          const late = this.computeCreditDeduction(stillLate);
-          underTime =
-            parseFloat(dailyTimeRec[0].lateMinutes || 0) - parseFloat(late);
-          console.log("TOTAL Repeated out: ", underTime);
-        } else {
-          underTime =
-            parseFloat(dailyTimeRec[0].lateMinutes || 0) +
-            parseFloat(conversionValue);
-          console.log("TOTAL First out: ", underTime);
-        }
-        if (underTime < 0) {
-          underTime = 0;
-        }
-
-        attendance.lateMinutes = underTime.toFixed(3).toString();
-        attendance.newlyHiredDeduct =
-          attendance.newlyHiredDeduct - this.computeCreditDeduction(stillLate);
-        attendance.stillLate = this.computeCreditDeduction(stillLate);
         attendance.isTimeOut = true;
         try {
           (data.logMessage = `Successfully Time Out ${empName}`),
